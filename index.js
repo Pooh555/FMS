90d4078700874cc389200802371fe262226bf71a
Pooh555 <peerasit.lu@gmail.com>
2024-10-11 15:03:55 +0700

    rtweah

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 // Image slideshow
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
-
-  for (i = 0; i < slides.length; i++) slides[i].style.display = "none";
-
-  slideIndex++;
-  if (slideIndex > slides.length) slideIndex = 1;
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000);
+  dots[slideIndex-1].className += " active";
 }
 
 // Authentication widget
