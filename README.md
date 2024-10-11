0b9386073232fcc6c9fbb0cfeba0cdbeec0b3896
Pooh555 <peerasit.lu@gmail.com>
2024-10-12 00:15:10 +0700

    Storing input login and registration data

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+PHdisk -------------------------------------------------------------------------------------
+
 This project is File Management System (FMS).
 
 The purpose of this project is to create a better alternative to Google Drive, Onedrive, etc for free.
--- index.js
+++ index.js
@@ -93,3 +93,86 @@ iconClose.addEventListener("click", () => {
     wrapper.classList.remove("active");
   }, 300);
 });
+
+let emailInput;
+let passwordInput;
+
+console.log("Email:", emailInput);
+console.log("Password:", passwordInput);
+
+class User {
+  constructor(username, password, email) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+
+  set username(newUsername) {
+    if (
+      newUsername != null &&
+      newUsername.length >= 3 &&
+      newUsername.length <= 12
+    )
+      this._username = newUsername;
+    else if (newUsername.length > 12)
+      console.error("Username must be no longer than 12 letters.");
+    else console.error("Username must be atleast 3 letters long.");
+  }
+
+  set password(password) {
+    if (newPassword != null && newPassword.length >= 8 && newPassword <= 20)
+      this._password = newPassword;
+    else if (newPassword.length >= 20)
+      console.error("Password must be no longer than 20 letters.");
+    else console.error("Password must be atleast 8 letters long.");
+  }
+
+  set email(newEmail) {
+    this._email = newEmail;
+  }
+
+  get username() {
+    return this._username;
+  }
+
+  get password() {
+    return this._password;
+  }
+
+  get email() {
+    return this._email;
+  }
+}
+
+const rememberMe = document.getElementById("rememberMe");
+const agreeToTerms = document.getElementById("agreeToTerms");
+
+login.onclick = function () {
+  // Evaluate checkbox status
+  if (rememberMe.checked) console.log("The data is stored.");
+  else console.log("The data will be forgotten.");
+
+  // Get email and password
+  emailInput = document.querySelector('input[type="email"]').value;
+  passwordInput = document.querySelector('input[type="password"]').value;
+
+  // Test
+  console.log(`email: ${emailInput}`);
+  console.log(`password: ${passwordInput}`);
+};
+
+register.onclick = function () {
+  // Evaluate checkbox status
+  if (agreeToTerms.checked) console.log("The user has agreed to the terms and conditions.");
+  else console.log("The user does not accept the terms and conditions.");
+
+  // Get username and password
+  emailInput = document.querySelector('input[type="email"]').value;
+  passwordInput = document.querySelector('input[type="password"]').value;
+
+  // Test
+  console.log(`email: ${emailInput}`);
+  console.log(`password: ${passwordInput}`);
+};
+
+const currentUser = new User(null, password, email);
