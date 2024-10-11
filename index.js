72709267d936cf3bf1942de4394ebf23cf39f2a6
Pooh555 <peerasit.lu@gmail.com>
2024-10-11 15:24:28 +0700

    slideshow position fixed

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // Image slideshow
 let slideIndex = 1;
+
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -16,14 +15,13 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+
+  for (i = 0; i < slides.length; i++) slides[i].style.display = "none";
+  for (i = 0; i < dots.length; i++) dots[i].className = dots[i].className.replace(" active", "");
+
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
