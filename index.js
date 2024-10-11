319c6fd5769e30017da531a36d76c8e7fe760bd6
Pooh555 <peerasit.lu@gmail.com>
2024-10-11 14:10:12 +0700

    Image slidewhow

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// Image slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+
+  for (i = 0; i < slides.length; i++) slides[i].style.display = "none";
+
+  slideIndex++;
+  if (slideIndex > slides.length) slideIndex = 1;
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
+
+// Authentication widget
 const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
