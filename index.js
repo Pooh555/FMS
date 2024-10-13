cbb2ee0119ddd42a384146c6f15bfb625f8cbfef
Pooh555 <peerasit.lu@gmail.com>
2024-10-13 22:55:55 +0700

    ip change

--- index.js
+++ index.js
@@ -296,7 +296,7 @@ function checkRegistration(
 }
 
 // Backend services
-const serverAddress = "http://localhost:3000/users";
+const serverAddress = "http://192.168.100.132:3000/users";
 
 // User authentication
 function authentication(user) {
